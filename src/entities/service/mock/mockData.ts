3421ecc8bd329ef96971ca2851291649419de702
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2025-12-22 06:52:48 -0500

    feat(service): add model and mock in service entity

---
+++ src/entities/service/mock/mockData.ts
@@ -0,0 +1,12 @@
+import { IService } from '@/entities/service/model/types';
+
+export const mockServices: Array<IService> = [
+  { id: 1, title: 'Historical Center Tour', price: 2500 },
+  { id: 2, title: 'Bus Sightseeing Tour', price: 3500 },
+  { id: 3, title: 'Art Museum Visit', price: 1500 },
+  { id: 4, title: 'Waterwalk', price: 4000 },
+  { id: 5, title: 'Gastronomic Tour', price: 4500 },
+  { id: 6, title: 'Evening Tour', price: 3000 },
+  { id: 7, title: 'Walking Tour', price: 2000 },
+  { id: 8, title: 'Nature Tour', price: 2800 }
+];

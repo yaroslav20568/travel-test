dab00893e704ca98462421910b2e95947887bf48
Yaroslav-salesautomators <yaraslav.r@salesautomators.com>
2025-12-22 12:13:35 -0500

    feat(general): add drawer functional in Card, add hooks

--- src/widgets/cart/ui/Cart.tsx
+++ src/widgets/cart/ui/Cart.tsx
@@ -2,32 +2,53 @@
 
 import { FC } from 'react';
 
+import { useCartMobileState } from '@/widgets/cart/hooks';
+
 import { CartHeader, useCartStore } from '@/features/cart';
 
 import { CartEmpty, CartList, CartSummary } from '@/entities/cart/ui';
 
-import { Line } from '@/shared';
+import { Line, useBodyScrollLock } from '@/shared';
 
 import s from './Cart.module.scss';
 
 export const Cart: FC = () => {
   const items = useCartStore(state => state.items);
+  const { isOpen, setIsOpen, isMobile } = useCartMobileState(items.length);
 
-  if (items.length === 0) {
+  useBodyScrollLock(isMobile && isOpen);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleOverlayClick = () => {
+    setIsOpen(false);
+  };
+
+  const cartContent =
+    items.length === 0 ? (
+      <CartEmpty />
+    ) : (
+      <>
+        <CartHeader onClose={isMobile ? handleClose : undefined} />
+        <Line />
+        <CartList />
+        <Line />
+        <CartSummary />
+      </>
+    );
+
+  if (isMobile) {
     return (
-      <div className={s.cart}>
-        <CartEmpty />
-      </div>
+      <>
+        {isOpen && <div className={s.overlay} onClick={handleOverlayClick} />}
+        <div className={`${s.cart} ${s.drawer} ${isOpen ? s.open : ''}`}>
+          {cartContent}
+        </div>
+      </>
     );
   }
 
-  return (
-    <div className={s.cart}>
-      <CartHeader />
-      <Line />
-      <CartList />
-      <Line />
-      <CartSummary />
-    </div>
-  );
+  return <div className={s.cart}>{cartContent}</div>;
 };

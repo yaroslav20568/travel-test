0b796f4bde7cd2e0239469f1b12ffb63ef876e6b
Yaroslav-salesautomators <yaraslav.r@salesautomators.com>
2025-12-22 16:23:25 -0500

    feat(general): implement infinite scroll with intersection observer, add reusable-hooks

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,2 +1,4 @@
 export { useBodyScrollLock } from './useBodyScrollLock';
+export { useInfiniteScroll } from './useInfiniteScroll';
+export { useIntersectionObserver } from './useIntersectionObserver';
 export { useIsMobile } from './useIsMobile';

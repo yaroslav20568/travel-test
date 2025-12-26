cea8d8993eea624c97c0b85234620c8bd91900dc
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2025-12-26 14:12:20 -0500

    fix(general): init playwright

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
